3e0da4b3cb4e283e65b9c4d8aa78a71b080f295f
Munyaradzi Gordon Muneka <gordonsnekaz@gmail.com>
2020-07-28 14:36:17 +0200

    Structure of auth

---
+++ app/src/main/java/zw/co/guava/soterio/ui/auth/VerifyOtp.kt
@@ -0,0 +1,12 @@
+package zw.co.guava.soterio.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import zw.co.guava.soterio.R
+
+class VerifyOtp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_otp)
+    }
+}
