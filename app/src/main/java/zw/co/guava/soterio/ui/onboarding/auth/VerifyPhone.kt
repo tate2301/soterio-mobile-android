d39cd032cd3d28cd72e3caaecebe0a1bfe36b919
Tatenda Christopher Chinyamakobvu <tatendachris@gmail.com>
2020-08-09 16:01:28 +0200

    Auth Frame Done

--- app/src/main/java/zw/co/guava/soterio/ui/onboarding/auth/VerifyPhone.kt
+++ app/src/main/java/zw/co/guava/soterio/ui/onboarding/auth/VerifyPhone.kt
@@ -1,20 +1,26 @@
 package zw.co.guava.soterio.ui.onboarding.auth
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.Html
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_verify_phone.*
 import zw.co.guava.soterio.R
-import zw.co.guava.soterio.ui.main_apk.MainActivity
+import java.util.concurrent.TimeUnit
 
 
 class VerifyPhone : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var storedVerificationId: String;
+    private lateinit var forceResendToken: PhoneAuthProvider.ForceResendingToken
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +28,18 @@ class VerifyPhone : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance();
         val currentUser: FirebaseUser? = mAuth.currentUser;
         if(currentUser != null) {
-            acknowledgeAuthentication(currentUser)
+            acknowledgeAuthenticationWithServer(currentUser)
         }
 
-        val phoneNumber = intent.getStringExtra(getString(R.string.phone_number))
+        val phoneNumber = intent.getStringExtra(getString(R.string.phone_number)).toString()
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber,
+            60,
+            TimeUnit.SECONDS,
+            this,
+            authCallbacks
+        )
 
         resendCodeLink.setOnClickListener {
             MaterialAlertDialogBuilder(this)
@@ -41,17 +55,80 @@ class VerifyPhone : AppCompatActivity() {
                 .show()
         }
 
+        // Start the verification process on code input
         verifyButton.setOnClickListener {
-            val intent = Intent(baseContext, VerifyAddress::class.java)
-            startActivity(intent)
-
+            val credential = PhoneAuthProvider.getCredential(storedVerificationId!!, otpInput.text.toString())
+            signInWithPhoneAuthCredential(credential)
         }
     }
 
-    private fun acknowledgeAuthentication(currentUser: FirebaseUser?) {
-        val intent = Intent(baseContext, MainActivity::class.java)
-        startActivity(intent)
+    private val authCallbacks = object: PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+            Log.d("FirebaseAuth", "OnVerificationCompleted:${p0}")
+            signInWithPhoneAuthCredential(p0);
+        }
+
+        override fun onVerificationFailed(p0: FirebaseException) {
+            Log.d("FirebaseAuth", "OnVerificationFailed: ${p0.message}")
+
+            if(p0 is FirebaseAuthInvalidCredentialsException) {
+                Log.d("FirebaseAuth", "OnVerificationFailed: FirebaseAuthInvalidCredentials")
+            } else if(p0 is FirebaseTooManyRequestsException) {
+                Log.d("FirebaseAuth", "OnVerificationFailed: FirebaseTooManyRequests: SMS Qouta")
+            }
+        }
+
+        // Verification code has been sent
+        override fun onCodeSent(verificationId: String, p1: PhoneAuthProvider.ForceResendingToken) {
+            super.onCodeSent(verificationId, p1)
+            Log.d("FirebaseAuth", "OnCodeSent:${verificationId}")
+
+            storedVerificationId = verificationId
+            forceResendToken = p1
+        }
+
     }
 
+    private fun signInWithPhoneAuthCredential(p0: PhoneAuthCredential) {
+        mAuth.signInWithCredential(p0)
+            .addOnCompleteListener {
+                if(it.isSuccessful) {
+                    Log.d("FirebaseAuth", "signInWithCredentialSuccess")
+
+                    // TODO - Authenticate with Server now
+
+                    val user = it.result?.user
+                    acknowledgeAuthenticationWithServer(user)
+
+                } else {
+                    Log.d("FirebaseAuth", "signInWithCredentialFailure", it.exception)
+                    if(it.exception is FirebaseAuthInvalidCredentialsException) {
+                        otpInput.error = "Wrong verification code! Please try again"
+                    }
+                }
+            }
+    }
+
+    private fun acknowledgeAuthenticationWithServer(currentUser: FirebaseUser?) {
+        val requestQueue = Volley.newRequestQueue(this)
+        val authRequest = StringRequest(Request.Method.POST,
+            getString(R.string.server_addr) + getString(R.string.route_auth),
+            Response.Listener<String> {
+                Log.d("ServerAuth", "OnAuthSuccess")
+            },
+            Response.ErrorListener {
+                Log.d("ServerAuth", "OnAuthFailure: ${it.message}")
+
+            })
+
+        requestQueue.add(authRequest)
+    }
+
+    private fun navigateToNextPage() {
+        //val intent = Intent(baseContext, VerifyAddress::class.java)
+        //startActivity(intent)
+    }
 
 }
+
+
